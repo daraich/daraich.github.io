11ec0a65d06e5c31ef8a6556cb3aee874f7f148b
Rachel Skrenta <rachelskrenta@gmail.com>
2020-08-17 11:48:53 -0700

    Testing Youtube embed

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 
   const CONTENT_ID = 'content';
   const contentSelector = document.getElementById(CONTENT_ID);
-
+  /*
   setInterval(() => {
     contentSelector.textContent = `Have fun! Lucky Number: ${Math.floor(Math.random() * 100)}`;
   }, 1000);
+  */
 })();
