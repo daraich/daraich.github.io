fcd7e7e6703ad4226e70e11e124a73b0d9398837
Rachel Skrenta <rachelskrenta@gmail.com>
2020-08-17 11:52:28 -0700

    Undo Youtube mebed testing

--- index.js
+++ index.js
@@ -3,9 +3,7 @@
 
   const CONTENT_ID = 'content';
   const contentSelector = document.getElementById(CONTENT_ID);
-  /*
   setInterval(() => {
     contentSelector.textContent = `Have fun! Lucky Number: ${Math.floor(Math.random() * 100)}`;
   }, 1000);
-  */
 })();
